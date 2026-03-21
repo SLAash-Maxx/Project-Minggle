06f6480ffbbd071f311853d3b661a26101693a84
Diluminda_Peiris <thelgedilu@gmail.com>
2026-03-21 13:19:59 +0530

    Fix namespace issue for google_ml_kit and legacy libraries
    Add namespace assignment for legacy library modules.

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -14,6 +14,22 @@ allprojects {
         google()
         mavenCentral()
     }
+
+    // Fix namespace issue for google_ml_kit and other legacy library modules
+    afterEvaluate {
+        if (pluginManager.hasPlugin("com.android.library")) {
+            val androidExtension = extensions.findByName("android")
+            if (androidExtension is com.android.build.gradle.LibraryExtension) {
+                if (androidExtension.namespace == null) {
+                    // Assign namespace based on project name
+                    androidExtension.namespace = when {
+                        project.name.contains("google_ml_kit") -> "com.b.biradar.google_ml_kit"
+                        else -> "com.minggle.lib.${project.name.replace("-", "").replace("_", "")}"
+                    }
+                }
+            }
+        }
+    }
 }
 
 val newBuildDir: Directory =
@@ -25,6 +41,10 @@ rootProject.layout.buildDirectory.value(newBuildDir)
 subprojects {
     val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
     project.layout.buildDirectory.value(newSubprojectBuildDir)
+}
+
+subprojects {
+    project.evaluationDependsOn(":app")
 }
 
 tasks.register<Delete>("clean") {
